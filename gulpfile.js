11f9f6cb2b1da7da7c2a3822e0fc5145f9d39272
DenisBiruk <denis.biruk.97@gmail.com>
2017-04-09 17:10:11 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglifyjs');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var del = require('del');
+var autoprefixer = require('gulp-autoprefixer');
+var jade = require('gulp-jade');
+
+gulp.task('sass', function () {
+  return gulp.src('app/sass/main.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({browsers: ['last 15 versions', '> 1%', 'ie 9'], cascade: true }))
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('jade', function () {
+  return gulp.src(['app/jade/*.jade', '!app/jade/**/_*.jade'])
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('app'))
+});
+
+gulp.task('scripts', function () {
+  return gulp.src(['app/js/**/*.js', '!app/js/common.min.js'])
+    .pipe(concat('common.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+    notify: false
+  });
+});
+
+gulp.task('clean', function () {
+  return del.sync('dist');
+});
+
+gulp.task('watch', ['browser-sync', 'sass', 'jade', 'scripts'], function () {
+  gulp.watch('app/sass/**/*.scss', ['sass']);
+  gulp.watch('app/jade/**/*.jade', ['jade']);
+  gulp.watch('app/js/**/*.js', ['scripts']);
+  gulp.watch('app/css/**/*.css', browserSync.reload);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('build', ['clean', 'sass', 'scripts'], function () {
+  var buildCSS = gulp.src('app/css/**/*.css')
+    .pipe(gulp.dest('dist/css'));
+
+  var buildFonts = gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+
+  var buildScripts = gulp.src('app/js/**/*.js')
+    .pipe(gulp.dest('dist/js'));
+
+  var buildImages = gulp.src('app/img/**/*')
+    .pipe(gulp.dest('dist/img'));
+
+  var buildHTML = gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
+});
